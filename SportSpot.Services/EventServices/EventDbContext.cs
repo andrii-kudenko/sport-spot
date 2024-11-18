1289acd45dc650bda8d842ad4a1e182f2daef21f
Phineas Luu <truongluu2703@gmail.com>
2024-11-18 10:05:21 -0500

    Event Services Added

---
+++ SportSpot.Services/EventServices/EventDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SportSpot.Entities;
+
+namespace SportSpot.Services.EventServices
+{
+	public class EventDbContext : DbContext
+	{
+
+		public EventDbContext(DbContextOptions<EventDbContext> options): base(options)
+		{
+
+		}
+
+		public DbSet<Event> Events { get; set; }
+	}
+}
+
---
+++ SportSpot.Services/EventServices/EventRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using SportSpot.Entities;
+
+namespace SportSpot.Services.EventServices
+{
+	public class EventRepository : IEventServices
+	{
+		public readonly EventDbContext _context;
+
+        public EventRepository(EventDbContext context)
+        {
+            _context = context;
+        }
+
+        // Get all Events
+        public List<Event> GetEvents()
+        {
+            return _context.Events.ToList();
+        }
+
+        // Get Event based on Id
+        public Event GetEvent(int id)
+        {
+            return _context.Events.FirstOrDefault(e => e.Id == id);
+        }
+
+        // Add event
+        public Event AddEvent(Event e)
+        {
+            _context.Events.Add(e);
+            _context.SaveChanges();
+            return e;
+        }
+
+        // Delete event
+        public string DeleteEvent(Event e)
+        {
+            _context.Events.Remove(e);
+            _context.SaveChanges();
+            return "Delete Successfully";
+        }
+
+        // Update Event
+        public Event UpdateEvent(Event e)
+        {
+            _context.Events.Update(e);
+            _context.SaveChanges();
+            return e;
+        }
+
+        // Get Event by Sports
+
+        // Get Event by Postal Code
+
+        // Get Event by City
+    }
+}
+
